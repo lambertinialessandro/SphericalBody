81e7dab8c0168c8076b99d45ae57d76000262870
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2023-04-29 18:37:25 +0200

    created temp pages

---
+++ src/Pages/Documentation/Documentation.js
@@ -0,0 +1,9 @@
+function Documentation() {
+  return (
+    <div>
+      <h1>Documentation</h1>
+    </div>
+  );
+}
+
+export default Documentation;
---
+++ src/Pages/Duo/Duo.js
@@ -0,0 +1,9 @@
+function Duo() {
+  return (
+    <div>
+      <h1>Duo</h1>
+    </div>
+  );
+}
+
+export default Duo;
